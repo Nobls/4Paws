ae242455565dca33f22e2823b94916cde0ef8bc1
Nabls <sergikon92@gmail.com>
2022-08-15 17:58:44 +0300

    changing design component

--- client/src/pages/Homepage/Homepage.tsx
+++ client/src/pages/Homepage/Homepage.tsx
@@ -4,11 +4,12 @@ import {Advantage} from "../../components/advantage/Advantage";
 import {LastNews} from "../../components/lastNews/LastNews";
 import Reviews from "../../components/reviews/Reviews";
 import Populars from "../../components/populars/Populars";
+import s from './homepage.module.scss'
 
 const Homepage = () => {
 
     return (
-        <div>
+        <div className={s.homepageBg}>
             <Banners/>
             <Advantage/>
             <LastNews/>
