63a328a8a2a8ec59b594f75050f6fd9eb0acf2e8
DzmitryR <dzmitryreshko@gmail.com>
2022-08-19 02:19:29 +0300

    add button in component AboutUs

--- client/src/pages/AboutUs/AboutUs.tsx
+++ client/src/pages/AboutUs/AboutUs.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import s from './aboutUs.module.scss';
+import paw from '../../images/Vector1.png';
 
 const AboutUs = () => {
     return (
         <div>
             <div className={s.aboutUsBg}>
                 <h1>Services</h1>
+                <a href="/">
+                    <p>Связаться с нами</p>
+                    <img src={paw} alt=""/>
+                </a>
             </div>
             <div>
                 <h1>Services</h1>
