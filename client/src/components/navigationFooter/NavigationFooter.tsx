af4446781f093dcc675ce60ee12936bc681ef1dc
DzmitryR <dzmitryreshko@gmail.com>
2022-08-25 00:12:29 +0300

    changing footer

--- client/src/components/navigationFooter/NavigationFooter.tsx
+++ client/src/components/navigationFooter/NavigationFooter.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import s from "./navigationFooter.module.scss";
+import {NavLink} from "react-router-dom";
 
 export const NavigationFooter = () => {
     return (
-        <div>
-
-        </div>
+        <nav className={s.containerNav}>
+            <h4>Ссылки</h4>
+            <div className={s.wrapper}>
+                <NavLink className={({isActive})=>isActive ? `${s.activeLink}`: `${s.link}`} to={'/'}>Главная</NavLink>
+                <NavLink className={({isActive})=>isActive ? `${s.activeLink}`: `${s.link}`} to={'/services'}>Услуги</NavLink>
+                <NavLink className={({isActive})=>isActive ? `${s.activeLink}`: `${s.link}`} to={'/shop'}>Магазин</NavLink>
+                <NavLink className={({isActive})=>isActive ? `${s.activeLink}`: `${s.link}`} to={'/news'}>Новости</NavLink>
+                <NavLink className={({isActive})=>isActive ? `${s.activeLink}`: `${s.link}`} to={'/about'}>О нас</NavLink>
+                <NavLink className={({isActive})=>isActive ? `${s.activeLink}`: `${s.link}`} to={'/contacts'}>Контакты</NavLink>
+            </div>
+        </nav>
     );
 };
