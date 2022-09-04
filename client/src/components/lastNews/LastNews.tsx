48bc89b5e290afbb7a9ff449da2f16ec3313d2f6
Nabls <sergikon92@gmail.com>
2022-09-04 20:37:26 +0300

    fixed styles in component lastNews, populars, homepage

--- client/src/components/lastNews/LastNews.tsx
+++ client/src/components/lastNews/LastNews.tsx
@@ -24,6 +24,7 @@ export const LastNews = () => {
                 </Link>
                 <Link to={'/news'} className={s.news2}>
                     <div className={s.newsItemWrapper}>
+                        <button>Открыть</button>
                         <img className={s.lastNewsImageTwo} src={lastNews[1].image} alt={lastNews[1].alt}/>
                         <h3 className={s.newsTitle}>{lastNews[1].title}</h3>
                         <p className={s.newsText}>{lastNews[1].description.substring(0, 100) + '...'}</p>
@@ -31,6 +32,7 @@ export const LastNews = () => {
                 </Link>
                 <Link to={'/news'} className={s.news3}>
                     <div className={s.newsItemWrapper}>
+                        <button>Открыть</button>
                         <img className={s.lastNewsImageThree} src={lastNews[2].image} alt={lastNews[2].alt}/>
                         <h3 className={s.newsTitle}>{lastNews[2].title}</h3>
                         <p className={s.newsText}>{lastNews[2].description.substring(0, 100) + '...'}</p>
@@ -38,6 +40,7 @@ export const LastNews = () => {
                 </Link>
                 <Link to={'/news'} className={s.news4}>
                     <div className={s.newsItemWrapper}>
+                        <button>Открыть</button>
                         <img className={s.lastNewsImageFour} src={lastNews[3].image} alt={lastNews[3].alt}/>
                         <h3 className={s.newsTitle}>{lastNews[3].title}</h3>
                         <p className={s.newsText}>{lastNews[3].description.substring(0, 100) + '...'}</p>
@@ -45,6 +48,7 @@ export const LastNews = () => {
                 </Link>
                 <Link to={'/news'} className={s.news5}>
                     <div className={s.newsItemWrapper}>
+                        <button>Открыть</button>
                         <img className={s.lastNewsImageFour} src={lastNews[4].image} alt={lastNews[4].alt}/>
                         <h3 className={s.newsTitle}>{lastNews[4].title}</h3>
                         <p className={s.newsText}>{lastNews[4].description.substring(0, 100) + '...'}</p>
