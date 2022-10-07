e84ba6d2084c2196a147c766c84804d301dfd978
DzmitryR <dzmitryreshko@gmail.com>
2022-10-07 15:37:09 +0300

    bag checkbox!

--- client/src/components/chooseServices/ChooseServices.tsx
+++ client/src/components/chooseServices/ChooseServices.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import s from './chooseServices.module.scss'
 import ButtonStandart from "../buttonStandart/ButtonStandart";
 import dogPhoto from '../../images/other/dog.png'
 import CustomCheckbox from "../customCheckbox/CustomCheckbox";
 
 const ChooseServices = () => {
+
+    const [checked, setChecked] = useState(false)
+
     return (
         <div className={s.chooseServicesContainer}>
             <h3 className={s.chooseServicesTitle}>Выбор Услуг</h3>
@@ -18,69 +21,71 @@ const ChooseServices = () => {
                     <div className={s.chooseServicesForm}>
                         <form className={s.labelWrapper}>
                             <label className={s.label}>
-                                <CustomCheckbox/>
+                                <CustomCheckbox checked={checked} setChecked={setChecked}/>
                                 <span className={s.labelTitle}>Выгул</span>
                             </label>
-                            <label className={s.labelAmount}> количество раз
-                                <select name={'amount'}>
+                            <label className={checked ? `${s.labelAmountActive}` : `${s.labelAmountDisabled}`}> количество раз
+                                <select className={s.servicesSelect} name={'amount'}>
                                     <option value="1">1</option>
                                     <option value="2">2</option>
+                                    <option value="3">3</option>
                                 </select></label>
-
                         </form>
                         <form className={s.labelWrapper}>
                             <label className={s.label}>
-                                <CustomCheckbox/>
+                                <CustomCheckbox checked={checked} setChecked={setChecked}/>
                                 <span className={s.labelTitle}>Кормление</span>
                             </label>
-                            <label className={s.labelAmount}> количество раз
-                                <select name={'amount'}>
+                            <label className={checked ? `${s.labelAmountActive}` : `${s.labelAmountDisabled}`}> количество раз
+                                <select className={s.servicesSelect} name={'amount'}>
                                     <option value="1">1</option>
                                     <option value="2">2</option>
+                                    <option value="3">3</option>
                                 </select></label>
 
                         </form>
                         <div className={s.labelWrapper}>
                             <label className={s.label}>
-                                <CustomCheckbox/>
+                                <CustomCheckbox checked={checked} setChecked={setChecked}/>
                                 <span className={s.labelTitle}>Уборка</span>
                             </label>
-                            <label className={s.labelAmount}> количество раз
-                                <select name={'amount'}>
+                            <label className={checked ? `${s.labelAmountActive}` : `${s.labelAmountDisabled}`}> количество раз
+                                <select className={s.servicesSelect} name={'amount'}>
                                     <option value="1">1</option>
                                     <option value="2">2</option>
+                                    <option value="3">3</option>
                                 </select></label>
 
                         </div>
                         <div className={s.labelWrapper}>
                             <label className={s.label}>
-                                <CustomCheckbox/>
+                                <CustomCheckbox checked={checked} setChecked={setChecked}/>
                                 <span className={s.labelTitle}>Сопровождение на груминг</span>
                             </label>
                         </div>
                         <div className={s.veterinarian}>
                             <div className={s.labelWrapper}>
                                 <label className={s.label}>
-                                    <CustomCheckbox/>
+                                    <CustomCheckbox checked={checked} setChecked={setChecked}/>
                                     <span className={s.labelTitle}>Сопровождение к ветеринару</span>
                                 </label>
                             </div>
                             <div className={s.veterinarianWrapper}>
                                 <div className={s.labelWrapper}>
                                     <label className={s.label}>
-                                        <CustomCheckbox/>
+                                        <CustomCheckbox checked={checked} setChecked={setChecked}/>
                                         <span className={s.labelTitle}>Операция</span>
                                     </label>
                                 </div>
                                 <div className={s.labelWrapper}>
                                     <label className={s.label}>
-                                        <CustomCheckbox/>
+                                        <CustomCheckbox checked={checked} setChecked={setChecked}/>
                                         <span className={s.labelTitle}>Прививка</span>
                                     </label>
                                 </div>
                                 <div className={s.labelWrapper}>
                                     <label className={s.label}>
-                                        <CustomCheckbox/>
+                                        <CustomCheckbox checked={checked} setChecked={setChecked}/>
                                         <span className={s.labelTitle}>Другие процедуры</span>
                                     </label>
                                 </div>
@@ -105,15 +110,20 @@ const ChooseServices = () => {
                             </div>
                         </div>
 
-                        <textarea className={s.servicesComment} cols={25} rows={5}
-                                  placeholder={'Комментарий'}>
-                        </textarea>
-
                     </div>
                 </div>
-                <div className={s.chooseServicesButton}>
-                    <ButtonStandart title={'Заказать'}/>
+
+
+                <div className={s.servicesCommentWrapper}>
+                    <textarea className={s.servicesComment} cols={25} rows={5}
+                              placeholder={'Комментарий'}>
+                    </textarea>
+                    <div className={s.chooseServicesButton}>
+                        <ButtonStandart title={'Заказать'}/>
+                    </div>
                 </div>
+
+
             </div>
         </div>
     );
--- client/src/components/customCheckbox/CustomCheckbox.tsx
+++ client/src/components/customCheckbox/CustomCheckbox.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {ChangeEvent} from 'react';
 import s from './customCheckbox.module.scss'
 
-const CustomCheckbox = () => {
+export type PropsType = {
+    checked: boolean
+    setChecked: (checked:boolean) => void
+}
+
+const CustomCheckbox = ({checked, setChecked}:PropsType) => {
+
+    const abc = (e:ChangeEvent<HTMLInputElement>) => {
+        let changeABC = e.target.checked;
+        setChecked(changeABC)
+        checked = changeABC;
+        console.log(checked)
+    }
+
     return (
         <label className={s.check}>
-            <input className={s.checkInput} type="checkbox"/>
+            <input className={s.checkInput} type="checkbox" onChange={abc}/>
             <span className={s.checkBox}>
             </span>
         </label>
