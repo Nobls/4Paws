cf7dadf5a72c64f5a901905ca1837365412a6c94
Nabls <sergikon92@gmail.com>
2022-10-26 01:37:19 +0300

    added routes for create post, update post, getAll post, getOne post, delete post, added post controller, post and login validations, added PostSchema

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import mongoose from "mongoose";
 
-import {registerValidation} from './validations/auth.js'
+import {registerValidation, loginValidation, postCreateValidation} from './validations/validations.js'
 import checkAuth from "./utils/checkAuth.js";
 
 import * as UserController from './controllers/UserController.js'
+import * as PostController from './controllers/PostController.js'
 
 mongoose
     .connect('mongodb+srv://AdminS:QQQwww444@pf.9ipuej5.mongodb.net/PF?retryWrites=true&w=majority')
@@ -15,11 +16,24 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/auth/login', UserController.login);
+app.post('/auth/login', loginValidation, UserController.login);
 
 app.post('/auth/registration', registerValidation, UserController.registration);
 
 app.get('/auth/me', checkAuth, UserController.getMe);
+
+
+
+
+app.get('/posts', PostController.getAll);
+
+app.get('/posts/:id', PostController.getOne);
+
+app.post('/posts', checkAuth, postCreateValidation, PostController.create);
+
+app.delete('/posts/:id', checkAuth, PostController.remove);
+
+app.patch('/posts/:id', checkAuth, PostController.update);
 
 app.listen(3157, (err) => {
     if (err) {
