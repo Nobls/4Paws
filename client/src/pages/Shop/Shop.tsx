beb1e51b6b72f4dc2fa81daaf41521c66643584b
DzmitryR <dzmitryreshko@gmail.com>
2022-11-29 23:51:22 +0300

    style shop

--- client/src/pages/Shop/Shop.tsx
+++ client/src/pages/Shop/Shop.tsx
@@ -1,11 +1,48 @@
 import React from 'react';
-import s from './shop.module.scss'
+import s from './shop.module.scss';
+import {Link} from "react-router-dom";
 
 const Shop = () => {
     return (
-        <div className={s.shopWrapper}>
+        <div className={s.shopBg}>
             <div className={s.shopContainer}>
+                <div className={s.shopWrapper}>
 
+                    <div className={s.shopSidebar}>
+                        <ul>
+                            <li>
+                                <Link to={'/'}>
+                                    Для собак
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to={'/'}>
+                                    Для котов
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to={'/'}>
+                                    Для грызунов
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to={'/'}>
+                                    Для птиц
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to={'/'}>
+                                    Для рыб
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div className={s.shopCategory}>
+
+                    </div>
+
+                </div>
             </div>
         </div>
     );
