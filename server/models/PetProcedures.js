4f2fe7e5f45c04fc07c62417a1dd61207dc3f8dd
Nabls <sergikon92@gmail.com>
2023-01-05 01:11:25 +0300

    additional commit

--- server/models/PetProcedures.js
+++ server/models/PetProcedures.js
@@ -4,33 +4,22 @@ const PetProceduresSchema = new mongoose.Schema(
     {
         typeVaccination: {
             type: String,
-            //required: true,
         },
         dateVaccination: {
             type: Date,
-            //required: true,
         },
         nameOfVaccine: {
             type: String,
-            //required: true,
         },
         dateProcedure: {
             type: Date,
-            //required: true,
         },
         nameOfProcedure: {
             type: String,
-            //required: true
         },
         nameClinic: {
             type: String,
-            //required: true,
         },
-        /*userPet: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'UserPet',
-            //required: true,
-        },*/
     },
     {
         timestamps: true,
