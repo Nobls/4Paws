bc1dbe9a8070f582cf2b9e03205ffdd2f0a82c8e
Nabls <sergikon92@gmail.com>
2023-01-06 14:52:28 +0300

    added functionality to add pets

--- client/src/pages/PetInfo/PetInfo.tsx
+++ client/src/pages/PetInfo/PetInfo.tsx
@@ -1,11 +1,62 @@
-import React, {useEffect} from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 import s from './petInfo.module.scss';
 import petPhoto from '../../images/pet.png';
 import ButtonStandart from "../../components/buttonStandart/ButtonStandart";
 import LowercaseButton from "../../components/lowercaseButton/LowercaseButton";
+import {useNavigate, useParams} from "react-router-dom";
+import axios from "../../axios/axios";
 
 
 const PetInfo = () => {
+
+    const params = useParams()
+
+    const navigate = useNavigate()
+
+    const [petName, setPetName] = useState('')
+    const [petBreed, setPetBreed] = useState('')
+    const [agePet, setAgePet] = useState('')
+    const [petGender, setPetGender] = useState('')
+    const [petAvatarUrl, setPetAvatarUrl] = useState('')
+
+    const [loading, setLoading] = useState(false)
+
+    const handleChangeFile = async (event: ChangeEvent<HTMLInputElement>) => {
+        try {
+            if (event.target.files !== null) {
+                const formData = new FormData()
+                const file = event.target.files[0]
+                formData.append('image', file)
+                const {data} = await axios.post('upload', formData)
+                setPetAvatarUrl(data.url)
+            }
+        } catch (err) {
+            console.warn(err)
+            alert('Ошибка при загрузке изображения!')
+        }
+    }
+
+    const removeImageHandler = () => {
+        setPetAvatarUrl('')
+    }
+
+    const onSubmit = async () => {
+        try {
+            setLoading(!loading)
+            const fieldsPet = {
+                petName,
+                petBreed,
+                agePet,
+                petGender,
+                petAvatarUrl,
+
+            }
+
+            //navigate(`/auth/user`)
+        } catch (err) {
+            console.log(err)
+        }
+    }
 
     useEffect(() => {
         window.scrollTo(0, 0);
@@ -13,28 +64,29 @@ const PetInfo = () => {
 
     const buttonTitleRegister = 'Сохранить';
 
-    const addText2 = () => {
-        console.log('Add Text2')
-    }
-
     return (
         <div className={s.petInfoBg}>
             <h1>Электронная карта питомца</h1>
             <div className={s.petInfoWrapper}>
-                <div className={s.petInfoItems}>
+                <form className={s.petInfoItems}>
                     <label>
                         <span>Кличка</span>
-                        <input type="text"/>
+                        <input value={petName || ''} type="text" onChange={e => setPetName(e.currentTarget.value)}/>
                     </label>
-                    <label><span>Порода</span><input type="text"/></label>
+                    <label>
+                        <span>Порода</span>
+                        <input value={petBreed || ''} type="text" onChange={e => setPetBreed(e.currentTarget.value)}/>
+                    </label>
                     <div className={s.petInfoAgeBlockItems}>
                         <label className={s.petInfoAgeBlockItemsWrapper}>
                             <span className={s.petInfoAgeBlockItemTitle}>Возраст</span>
-                            <input className={s.petInfoAgeBlockItemInput} type="number"/>
+                            <input value={agePet || ''} className={s.petInfoAgeBlockItemInput} type="text"
+                                   onChange={e => setAgePet(e.currentTarget.value)}/>
                         </label>
                         <label className={s.petInfoAgeBlockItemsWrapper}>
                             <span className={s.petInfoSexBlockItemTitle}>Пол</span>
-                            <select className={s.petInfoSexBlockItemInput} name="pet">
+                            <select value={petGender || ''} className={s.petInfoSexBlockItemInput} name="pet"
+                                    onChange={e => setPetGender(e.currentTarget.value)}>
                                 <option value="m">М</option>
                                 <option value="w">Ж</option>
                             </select>
@@ -47,14 +99,30 @@ const PetInfo = () => {
                         </label>
                     </div>
 
-                </div>
+                </form>
 
                 <div className={s.petPhotoWrapper}>
+                    {
+                        petAvatarUrl && (
+                            <>
+                                {
+                                    petAvatarUrl
+                                        ?
+                                        <img className={s.petPhoto} src={`http://localhost:3157${petAvatarUrl}`}
+                                             alt="pet"/>
+                                        :
+                                        <img className={s.petPhoto} src={petPhoto} alt="pet"/>
+                                }
+                                <button onClick={removeImageHandler}>Удалить</button>
+                            </>
+                        )
+                    }
                     <img className={s.petPhoto} src={petPhoto} alt="pet"/>
+                    <input type="file" onChange={handleChangeFile}/>
                     <div className={s.buttonWrapper}>
                         <LowercaseButton title={'Добавить фото'}/>
                     </div>
-                    <ButtonStandart title={buttonTitleRegister} onclick={addText2}/>
+                    <ButtonStandart title={buttonTitleRegister} onclick={onSubmit}/>
                 </div>
             </div>
         </div>
