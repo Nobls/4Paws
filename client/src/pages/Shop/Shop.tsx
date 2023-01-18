1297fa8df431a27893c59865bae132792cfced6a
DzmitryR <dzmitryreshko@gmail.com>
2023-01-19 00:13:15 +0300

    delete style shop component

--- client/src/pages/Shop/Shop.tsx
+++ client/src/pages/Shop/Shop.tsx
@@ -1,21 +1,21 @@
-import React, {useState} from 'react';
+// import React, {useState} from 'react';
 import s from './shop.module.scss';
 // import ShopSidebar from "../../components/shopSidebar/ShopSidebar";
 // import ShopCategory from "../../components/shopCategory/ShopCategory";
-import dogCard from '../../images/shopPetCard/dog001.png';
-//import catCard from '../../images/shopPetCard/cat002.png';
-import {Link} from "react-router-dom";
-import dogEat from '../../images/user.png';
+// import dogCard from '../../images/shopPetCard/dog001.png';
+// import catCard from '../../images/shopPetCard/cat002.png';
+// import {Link} from "react-router-dom";
+// import dogEat from '../../images/user.png';
 
 
 const Shop = () => {
 
-    const [menu, setMenu] = useState(false);
-
-    const changeMenu = () => {
-        setMenu(!menu);
-        console.log(menu)
-    }
+    // const [menu, setMenu] = useState(false);
+    //
+    // const changeMenu = () => {
+    //     setMenu(!menu);
+    //     console.log(menu)
+    // }
 
 
     return (
@@ -23,33 +23,33 @@ const Shop = () => {
             <div className={s.shopContainer}>
                 <div className={s.shopWrapper}>
 
-                    <div className={s.shopPetsCardWrapperDog} style= {menu ? {backgroundColor: 'coral'} : {backgroundImage: `url(${dogCard})`}} onClick={changeMenu}>
+                    {/*<div className={s.shopPetsCardWrapperDog} style= {menu ? {backgroundColor: 'coral'} : {backgroundImage: `url(${dogCard})`}} onClick={changeMenu}>*/}
 
-                        <div className={menu ? `${s.shopDogsActive}` : `${s.shopDogs}`}>
-                            <p className={s.shopPetsTitle}>Для собак</p>
-                        </div>
+                    {/*    <div className={menu ? `${s.shopDogsActive}` : `${s.shopDogs}`}>*/}
+                    {/*        <p className={s.shopPetsTitle}>Для собак</p>*/}
+                    {/*    </div>*/}
 
-                        <div className={menu ? `${s.dogEatActive} ${s.shopDogsActive}` : `${s.dogEat} ${s.shopDogs}`}>
-                            <div className={s.dogItems}>
-                                <Link className={s.dogItem} to={'/'}>
-                                    <img className={s.shopCategoryIcon} src={dogEat} alt="dogEat"/>
-                                    <p className={s.dogItemTitle}>Корма</p>
-                                </Link>
-                                <Link className={s.dogItem} to={'/'}>
-                                    <img className={s.shopCategoryIcon} src={dogEat} alt="dogEat"/>
-                                    <p className={s.dogItemTitle}>Наполнители</p>
-                                </Link>
-                                <Link className={s.dogItem} to={'/'}>
-                                    <img className={s.shopCategoryIcon} src={dogEat} alt="dogEat"/>
-                                    <p className={s.dogItemTitle}>Аксессуары</p>
-                                </Link>
-                                <Link className={s.dogItem} to={'/'}>
-                                    <img className={s.shopCategoryIcon} src={dogEat} alt="dogEat"/>
-                                    <p className={s.dogItemTitle}>Аксессуары</p>
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
+                    {/*    <div className={menu ? `${s.dogEatActive} ${s.shopDogsActive}` : `${s.dogEat} ${s.shopDogs}`}>*/}
+                    {/*        <div className={s.dogItems}>*/}
+                    {/*            <Link className={s.dogItem} to={'/'}>*/}
+                    {/*                <img className={s.shopCategoryIcon} src={dogEat} alt="dogEat"/>*/}
+                    {/*                <p className={s.dogItemTitle}>Корма</p>*/}
+                    {/*            </Link>*/}
+                    {/*            <Link className={s.dogItem} to={'/'}>*/}
+                    {/*                <img className={s.shopCategoryIcon} src={dogEat} alt="dogEat"/>*/}
+                    {/*                <p className={s.dogItemTitle}>Наполнители</p>*/}
+                    {/*            </Link>*/}
+                    {/*            <Link className={s.dogItem} to={'/'}>*/}
+                    {/*                <img className={s.shopCategoryIcon} src={dogEat} alt="dogEat"/>*/}
+                    {/*                <p className={s.dogItemTitle}>Аксессуары</p>*/}
+                    {/*            </Link>*/}
+                    {/*            <Link className={s.dogItem} to={'/'}>*/}
+                    {/*                <img className={s.shopCategoryIcon} src={dogEat} alt="dogEat"/>*/}
+                    {/*                <p className={s.dogItemTitle}>Аксессуары</p>*/}
+                    {/*            </Link>*/}
+                    {/*        </div>*/}
+                    {/*    </div>*/}
+                    {/*</div>*/}
 
 
                     {/*<div className={s.shopPetsCardWrapperCat} style={{backgroundColor: `brown`}}>*/}
@@ -67,10 +67,6 @@ const Shop = () => {
                     {/*<div className={s.shopPetsCardWrapper} style={{backgroundColor: 'green'}}>*/}
                     {/*    <p className={s.shopPetsTitle}>Для рыб</p>*/}
                     {/*</div>*/}
-
-
-
-
 
 
 
@@ -94,6 +90,18 @@ const Shop = () => {
                     {/*    </div>*/}
 
                     {/*</div>*/}
+
+
+
+                    {/* Нужно делать 2 компоненты-карточки.
+                    Через UseState на onclick вешаем тернарку, либо загружается компонента 1 либо компонента 2.
+                     Компоненте 1 передаётся title для кого
+                     Компоненте 2 передаётся список категорий товаров.
+                     При нажатии на компоненту 2 открывается новая страница - список товаров*/}
+
+
+
+
 
                 </div>
             </div>
