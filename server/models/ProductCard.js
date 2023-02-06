d4079585c8168e721353f830deacaf1351f3aa77
DzmitryR <dzmitryreshko@gmail.com>
2023-02-06 23:49:24 +0300

    refactor code

--- server/models/ProductCard.js
+++ server/models/ProductCard.js
@@ -3,14 +3,14 @@ import mongoose from "mongoose";
 
 const ProductCardSchema = new mongoose.Schema(
     {
-        title: {type: String},
-        productImage: {type: String},
-        weight: {type: String},
-        size: {type: String},
-        type: {type: String},
-        countryOfManufacture: {type: String},
-        description: {type: String},
-        price: {type: Number},
+        title: String,
+        productImage: String,
+        weight: String,
+        size: String,
+        type: String,
+        countryOfManufacture: String,
+        description: String,
+        price: Number,
     },
     {
             timestamps: true
