9f208256bcb9c5fdb6c1e24b67a40a85213a289c
DzmitryR <dzmitryreshko@gmail.com>
2023-03-01 15:21:45 +0300

    added get in CategoryRodentsController

--- server/controllers/CategoryRodentsController.js
+++ server/controllers/CategoryRodentsController.js
@@ -285,3 +285,203 @@ export const createDishesForRodents = async (req, res) => {
         })
     }
 }
+
+
+
+export const getDryFoodForRodents = async (req, res, next) => {
+    try {
+        const {dryFoodForRodentsId} = req.params;
+
+        const productForRodents = await ProductsForRodentModel.findOne({
+            "dryFoodForRodents._id": dryFoodForRodentsId
+        });
+
+        if (!productForRodents) {
+            return res.status(404).json({error: "Product for rodents not found"});
+        }
+
+        const dryFoodForRodentsProducts = await Promise.all(
+            productForRodents.dryFoodForRodents.product.map((p) => {
+                return ProductCardModel.findById(p);
+            })
+        );
+
+        res.status(200).json(dryFoodForRodentsProducts);
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const getVitaminsForRodents = async (req, res, next) => {
+    try {
+        const {vitaminsForRodentsId} = req.params;
+
+        const productForRodents = await ProductsForRodentModel.findOne({
+            "vitaminsForRodents._id": vitaminsForRodentsId
+        });
+
+        if (!productForRodents) {
+            return res.status(404).json({error: "Product for rodents not found"});
+        }
+
+        const vitaminsForRodentsProducts = await Promise.all(
+            productForRodents.vitaminsForRodents.product.map((p) => {
+                return ProductCardModel.findById(p);
+            })
+        );
+
+        res.status(200).json(vitaminsForRodentsProducts);
+    } catch (error) {
+        next(error);
+    }
+}
+
+
+export const getAccessoriesForRodents = async (req, res, next) => {
+    try {
+        const {accessoriesForRodentsId} = req.params;
+
+        const productForRodents = await ProductsForRodentModel.findOne({
+            "accessoriesForRodents._id": accessoriesForRodentsId
+        });
+
+        if (!productForRodents) {
+            return res.status(404).json({error: "Product for rodents not found"});
+        }
+
+        const accessoriesForRodentsProducts = await Promise.all(
+            productForRodents.accessoriesForRodents.product.map((p) => {
+                return ProductCardModel.findById(p);
+            })
+        );
+
+        res.status(200).json(accessoriesForRodentsProducts);
+    } catch (error) {
+        next(error);
+    }
+}
+
+
+export const getCellsForRodents = async (req, res, next) => {
+    try {
+        const {cellsForRodentsId} = req.params;
+
+        const productForRodents = await ProductsForRodentModel.findOne({
+            "cellsForRodents._id": cellsForRodentsId
+        });
+
+        if (!productForRodents) {
+            return res.status(404).json({error: "Product for rodents not found"});
+        }
+
+        const cellsForRodentsProducts = await Promise.all(
+            productForRodents.cellsForRodents.product.map((p) => {
+                return ProductCardModel.findById(p);
+            })
+        );
+
+        res.status(200).json(cellsForRodentsProducts);
+    } catch (error) {
+        next(error);
+    }
+}
+
+
+export const getBathingForRodents = async (req, res, next) => {
+    try {
+        const {bathingForRodentsId} = req.params;
+
+        const productForRodents = await ProductsForRodentModel.findOne({
+            "bathingForRodents._id": bathingForRodentsId
+        });
+
+        if (!productForRodents) {
+            return res.status(404).json({error: "Product for rodents not found"});
+        }
+
+        const bathingForRodentsProducts = await Promise.all(
+            productForRodents.bathingForRodents.product.map((p) => {
+                return ProductCardModel.findById(p);
+            })
+        );
+
+        res.status(200).json(bathingForRodentsProducts);
+    } catch (error) {
+        next(error);
+    }
+}
+
+
+export const getToysForRodents = async (req, res, next) => {
+    try {
+        const {toysForRodentsId} = req.params;
+
+        const productForRodents = await ProductsForRodentModel.findOne({
+            "toysForRodents._id": toysForRodentsId
+        });
+
+        if (!productForRodents) {
+            return res.status(404).json({error: "Product for rodents not found"});
+        }
+
+        const toysForRodentsProducts = await Promise.all(
+            productForRodents.toysForRodents.product.map((p) => {
+                return ProductCardModel.findById(p);
+            })
+        );
+
+        res.status(200).json(toysForRodentsProducts);
+    } catch (error) {
+        next(error);
+    }
+}
+
+
+export const getExtenderForRodents = async (req, res, next) => {
+    try {
+        const {extenderForRodentsId} = req.params;
+
+        const productForRodents = await ProductsForRodentModel.findOne({
+            "extenderForRodents._id": extenderForRodentsId
+        });
+
+        if (!productForRodents) {
+            return res.status(404).json({error: "Product for rodents not found"});
+        }
+
+        const extenderForRodentsProducts = await Promise.all(
+            productForRodents.extenderForRodents.product.map((p) => {
+                return ProductCardModel.findById(p);
+            })
+        );
+
+        res.status(200).json(extenderForRodentsProducts);
+    } catch (error) {
+        next(error);
+    }
+}
+
+
+export const getDishesForRodents = async (req, res, next) => {
+    try {
+        const {dishesForRodentsId} = req.params;
+
+        const productForRodents = await ProductsForRodentModel.findOne({
+            "dishesForRodents._id": dishesForRodentsId
+        });
+
+        if (!productForRodents) {
+            return res.status(404).json({error: "Product for rodents not found"});
+        }
+
+        const dishesForRodentsProducts = await Promise.all(
+            productForRodents.dishesForRodents.product.map((p) => {
+                return ProductCardModel.findById(p);
+            })
+        );
+
+        res.status(200).json(dishesForRodentsProducts);
+    } catch (error) {
+        next(error);
+    }
+}
