6a054b608f41e55a75c734ecca01ca8491d78c5c
Nabls <sergikon92@gmail.com>
2023-03-10 23:36:11 +0300

    refactoring code

--- server/models/Post.js
+++ server/models/Post.js
@@ -56,6 +56,10 @@ const PostSchema = new mongoose.Schema(
             default: 0
         },
 
+        publicationDate:{
+            type: Date
+        },
+
         user: {
             type: mongoose.Schema.Types.ObjectId,
             ref: 'User',
