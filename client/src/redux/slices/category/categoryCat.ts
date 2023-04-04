31a693cb6f1aea0df34ab8915b3418d5c2d75483
Nabls <sergikon92@gmail.com>
2023-04-04 23:54:00 +0300

    refactoring code

--- client/src/redux/slices/category/categoryCat.ts
+++ client/src/redux/slices/category/categoryCat.ts
@@ -16,111 +16,11 @@ export interface ProductCard {
     tags: string,
 }
 
-/*export interface CategoryDog {
-    _id: string;
-    product: ProductCard[];
-    loading: boolean;
-    errors: string | null;
-}*/
-
-/*export interface AllCategoryDog {
-    dryFoodForDogs: CategoryDog
-    preservesForDogs: CategoryDog
-    vitaminsForDogs: CategoryDog
-    homeForDogs: CategoryDog
-    toysForDogs: CategoryDog
-    carryingForDogs: CategoryDog
-    cosmeticsForDogs: CategoryDog
-    clothesForDogs: CategoryDog
-    dishesForDogs: CategoryDog
-    ammunitionForDogs: CategoryDog
-    toiletsForDogs: CategoryDog
-    loading: boolean
-    errors: any
-}*/
-
-/*interface ProductsState {
-    products: CategoryDog | null;
-    loading: boolean;
-    error: string | null;
-}*/
-
 interface ProductsState {
     products: ProductCard[];
     loading: boolean;
     error: string | null;
 }
-
-/*const initialState: AllCategoryDog = {
-    dryFoodForDogs: {
-        _id: '',
-        product: [],
-        loading: false,
-        errors: null,
-    },
-    preservesForDogs: {
-        _id: '',
-        product: [],
-        loading: false,
-        errors: null,
-    },
-    vitaminsForDogs: {
-        _id: '',
-        product: [],
-        loading: false,
-        errors: null,
-    },
-    homeForDogs: {
-        _id: '',
-        product: [],
-        loading: false,
-        errors: null,
-    },
-    toysForDogs: {
-        _id: '',
-        product: [],
-        loading: false,
-        errors: null,
-    },
-    carryingForDogs: {
-        _id: '',
-        product: [],
-        loading: false,
-        errors: null,
-    },
-    cosmeticsForDogs: {
-        _id: '',
-        product: [],
-        loading: false,
-        errors: null,
-    },
-    clothesForDogs: {
-        _id: '',
-        product: [],
-        loading: false,
-        errors: null,
-    },
-    dishesForDogs: {
-        _id: '',
-        product: [],
-        loading: false,
-        errors: null,
-    },
-    ammunitionForDogs: {
-        _id: '',
-        product: [],
-        loading: false,
-        errors: null,
-    },
-    toiletsForDogs: {
-        _id: '',
-        product: [],
-        loading: false,
-        errors: null,
-    },
-    loading: false,
-    errors: null,
-}*/
 
 const initialState: ProductsState = {
     products: [],
