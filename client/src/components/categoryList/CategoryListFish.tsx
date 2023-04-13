7388542675d8ae9f53a50451eac07518b39a39ce
Nabls <sergikon92@gmail.com>
2023-04-13 18:18:18 +0300

    added component ProductFish, routs for fish

--- client/src/components/categoryList/CategoryListFish.tsx
+++ client/src/components/categoryList/CategoryListFish.tsx
@@ -11,37 +11,37 @@ const CategoryListDog = ({data}:PropsType) => {
         <div className={s.categoryListWrapper}>
             <ul className={s.listItems}>
                 <li className={s.listItem}>
-                    <Link className={s.listItemLink} to={'/'}>
+                    <Link className={s.listItemLink} to={`/shop/fish/aquariums/${data[0].aquariums._id}`}>
                         Аквариумы
                     </Link>
                 </li>
                 <li className={s.listItem}>
-                    <Link className={s.listItemLink} to={'/'}>
+                    <Link className={s.listItemLink} to={`/shop/fish/aquariums/${data[0].livingInhabitants._id}`}>
                         Живые обитатели
                     </Link>
                 </li>
                 <li className={s.listItem}>
-                    <Link className={s.listItemLink} to={'/'}>
+                    <Link className={s.listItemLink} to={`/shop/fish/aquariums/${data[0].aquariumsAndPedestals._id}`}>
                         Аквариумы и тумбы
                     </Link>
                 </li>
                 <li className={s.listItem}>
-                    <Link className={s.listItemLink} to={'/'}>
+                    <Link className={s.listItemLink} to={`/shop/fish/aquariums/${data[0].fishFood._id}`}>
                         Корма
                     </Link>
                 </li>
                 <li className={s.listItem}>
-                    <Link className={s.listItemLink} to={'/'}>
+                    <Link className={s.listItemLink} to={`/shop/fish/aquariums/${data[0].decorations._id}`}>
                         Декорации
                     </Link>
                 </li>
                 <li className={s.listItem}>
-                    <Link className={s.listItemLink} to={'/'}>
+                    <Link className={s.listItemLink} to={`/shop/fish/aquariums/${data[0].equipment._id}`}>
                         Оборудование
                     </Link>
                 </li>
                 <li className={s.listItem}>
-                    <Link className={s.listItemLink} to={'/'}>
+                    <Link className={s.listItemLink} to={`/shop/fish/aquariums/${data[0].chemistryAndMedicines._id}`}>
                         Химия и лекарства
                     </Link>
                 </li>
---
+++ client/src/pages/Product/ProductFish.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {useAppDispatch} from "../../redux/hook/hook";
+
+export const ProductFish = () => {
+
+    const dispatch = useAppDispatch()
+
+
+    return (
+        <div>
+
+        </div>
+    );
+};
