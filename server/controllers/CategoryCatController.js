7a98d1dbbb8f6158e47b141e87a38f5f9c08a424
Nabls <sergikon92@gmail.com>
2023-05-08 23:17:39 +0300

    fix

--- server/controllers/CategoryCatController.js
+++ server/controllers/CategoryCatController.js
@@ -409,11 +409,11 @@ export const getDryFoodForCats = async (req, res, next) => {
 }
 
 export const getCategoryDryFoodForCatsById = async (req, res) => {
-    const {dryFoodCatsId} = req.params;
+    const {dryFoodForCatsId} = req.params;
 
     try {
         const product = await ProductsForCatsModel.findOne({
-            'dryFoodForCats._id': dryFoodCatsId
+            'dryFoodForCats._id': dryFoodForCatsId
         }).select('dryFoodForCats.category');
 
         if (product) {
