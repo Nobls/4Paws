354cd2cf3768ed0d0bd0011b04118e5bca0db199
Nabls <sergikon92@gmail.com>
2023-05-13 23:55:41 +0300

    .

--- client/src/redux/slices/category/categoryBird.ts
+++ client/src/redux/slices/category/categoryBird.ts
@@ -16,8 +16,14 @@ export interface ProductCard {
     tags: string,
 }
 
+type CategoryObject = {
+    category: string;
+}
+
+
 interface ProductsState {
     products: ProductCard[];
+    category: CategoryObject;
     loading: boolean;
     error: string | null;
 }
@@ -25,6 +31,9 @@ interface ProductsState {
 
 const initialState: ProductsState = {
     products: [],
+    category: {
+        category: ''
+    },
     loading: false,
     error: null,
 };
